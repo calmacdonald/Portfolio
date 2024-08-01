1bd1ee83f31ba3df2f643249f9721e98010b9b42
Callum MacDonald <callumian@live.com>
2024-08-01 16:30:55 -0700

    Added animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ function headerShadow() {
     const navHeader = document.getElementById("header");
 
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-        navHeader.style.boxShadow = "0 1px 6px rbga(0, 0, 0, 0.1)";
+        navHeader.style.boxShadow = "0 2px 6px rgba(0, 0, 0, 0.3)";
         navHeader.style.height = "70px";
         navHeader.style.lineHeight = "70px";
 
@@ -27,3 +27,43 @@ function headerShadow() {
 
     }
 }
+
+var typingEffect = new Typed(".typedText",{
+    strings : ["Coder", "Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured_name', {delay: 100})
+sr.reveal('.featured-text-info', {delay: 200})
+sr.reveal('.social_icons', {delay: 300})
+sr.reveal('.featured-image', {delay: 400})
+sr.reveal('.top-header',{})
+
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srLeft.reveal('.about-info',{})
+
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srRight.reveal('.skills-box',{})
